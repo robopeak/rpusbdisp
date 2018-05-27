0315643a9d6c23d6f307a8beb5a619d67c8b682d
Forest Godfrey <fgodfrey@bigw.org>
2018-05-27 06:32:28 +0000

    Fix another compiler warning.

--- drivers/linux-driver/src/usbhandlers.c
+++ drivers/linux-driver/src/usbhandlers.c
@@ -932,7 +932,7 @@ static int _on_alloc_disp_tickets_pool(struct rpusbdisp_dev * dev)
     dev->disp_tickets_pool.disp_urb_count = actual_allocated;
     dev->disp_tickets_pool.availiable_count = actual_allocated;
     dev->disp_tickets_pool.packet_size_factor = packet_size_factor;
-    dev_info(&dev->interface->dev, "allocated %d urb tickets for transfering display data. %lu size each\n", actual_allocated, ticket_logic_size);
+    dev_info(&dev->interface->dev, "allocated %d urb tickets for transfering display data. %zu size each\n", actual_allocated, ticket_logic_size);
 
     return actual_allocated?0:-ENOMEM;
 };
